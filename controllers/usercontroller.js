d55a980390cb2a512be5957844f804b5b14b574a
aadil-ansari-rd <aadilansari.rd@gmail.com>
2024-09-16 11:07:48 +0530

    Updation

---
+++ controllers/usercontroller.js
@@ -0,0 +1,16 @@
+const users = require('../users.json');
+function getUser(req, res){
+    try{
+        res.json(users);
+    }catch(err){
+        console.log(err);
+    }
+}
+// try{
+
+// }catch(err){
+//     console.log(err);
+// }
+module.exports = {
+    getUser
+}
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const user = require('./routes/user')
+const app = express();
+app.use(user);
+
+
+
+
+app.listen(4024,(err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Server is running on port 4024");
+    }
+})
